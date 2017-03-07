f85cebe310141613ef5bd9e43a9d227951f7e77c
Kane Balagtey <kbalagtey@tacitknowledge.com>
2017-03-07 08:50:54 +0000

    move index.js to src directory

--- index.js
+++
@@ -1,9 +0,0 @@
-import Form from 'src/forms/form';
-import FormGroup from 'src/forms/formGroup';
-import FormError from 'src/forms/formError';
-import * as FormActions from 'src/forms/formActions';
-
-export FormActions;
-export FormError;
-export FormGroup;
-export default Form;
