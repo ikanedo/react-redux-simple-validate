b7dcf2807262bc3e91016227be3f4929cf8d7cd9
Kane Balagtey <kbalagtey@tacitknowledge.com>
2017-03-09 18:06:53 +0000

    update index.js references for import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Form from './forms/form';
-import FormGroup from './forms/form';
-import FormGroupAsync from './forms/form';
-import FormError from './forms/form';
-import FormReducer from './forms/form';
+import FormGroup from './forms/form/formGroup';
+import FormGroupAsync from './forms/form/formGroupAsync';
+import FormError from './forms/form/formError';
+import FormReducer from './forms/form/formReducer';
 
 export { Form, FormGroup, FormGroupAsync, FormError, FormReducer };
