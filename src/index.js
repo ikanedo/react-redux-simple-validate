82c47a42a1381808989492b9ae6e262d70dc26e9
Kane Balagtey <kbalagtey@tacitknowledge.com>
2017-03-09 20:13:12 +0000

    export form actions

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import FormGroup from './forms/formGroup';
 import FormGroupAsync from './forms/formGroupAsync';
 import FormError from './forms/formError';
 import FormReducer from './forms/formReducer';
+import * as FormActions from './forms/formActions';
 
-export { Form, FormGroup, FormGroupAsync, FormError, FormReducer };
+export { Form, FormGroup, FormGroupAsync, FormError, FormReducer, FormActions };
